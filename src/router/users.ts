be6564063d4389f0108b505776efddc13f9a908b
kjun <kjun_all@naver.com>
2023-04-15 03:20:53 +0900

    :sparkles: create router

---
+++ src/router/users.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserController } from "../controller/UserController";
+
+const router = Router();
+
+router.get("/", UserController.getUser);
+router.post("/", UserController.createUser);
+
+export default router;
