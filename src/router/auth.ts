58f4f41d3db6def555684e5df11f04db7ac6c5f1
yousunzoo <dpfvmffjqm@gmail.com>
2023-04-15 20:22:47 +0900

    :fire: 서버 초기화

--- src/router/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserController } from "../controller/UserController";
-
-const routes = Router();
-
-routes.post("/register", UserController.register);
-routes.post("/login", UserController.login);
-export default routes;
