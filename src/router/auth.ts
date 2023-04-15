b047c8f67085b724d0ebb6690e442f83ec3ae583
yousunzoo <dpfvmffjqm@gmail.com>
2023-04-16 00:26:12 +0900

    :sparkles: 회원가입에 프로필사진 추가 기능 구현

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -5,7 +5,7 @@ import { upload } from '../middleware/uploadS3';
 
 const routes = Router();
 
-routes.post('/register', UserController.register);
+routes.post('/register', upload.single('image'), UserController.register);
 routes.post('/check', UserController.checkDuplicate);
 routes.post('/login', UserController.login);
 routes.post('/logout', UserController.logout);
