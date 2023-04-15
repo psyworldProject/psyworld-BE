641b6216e9389e508e073929523b0db85e14f75e
yousunzoo <dpfvmffjqm@gmail.com>
2023-04-16 01:22:47 +0900

    :sparkles: 방명록 작성 기능 구현

--- src/controller/GuestbookController.ts
+++ src/controller/GuestbookController.ts
@@ -2,6 +2,7 @@ import { Guestbook } from './../entity/Guestbook';
 import { Request, Response } from 'express';
 import { myDataBase } from '../db';
 import { User } from '../entity/User';
+import { JwtRequest } from '../middleware/AuthMiddleware';
 
 export class GuestbookController {
 	static getGuestbook = async (req: Request, res: Response) => {
@@ -18,5 +19,29 @@ export class GuestbookController {
 			relations: ['owner'],
 		});
 		res.status(200).json({ guestbook });
+	};
+
+	static createGuestbook = async (req: JwtRequest, res: Response) => {
+		const { content, ownerId } = req.body;
+		const { id: userId } = req.decoded;
+
+		const owner = await myDataBase.getRepository(User).findOne({
+			where: { id: ownerId },
+		});
+		const author = await myDataBase.getRepository(User).findOne({
+			where: { id: userId },
+		});
+		if (!owner || !author) {
+			return res.status(404).send({ message: '해당 유저를 찾을 수 없습니다.' });
+		}
+
+		const guestbook = new Guestbook();
+		guestbook.content = content;
+		guestbook.owner = owner;
+		guestbook.author = author;
+
+		const result = await myDataBase.getRepository(Guestbook).insert(guestbook);
+
+		res.status(201).json({ result });
 	};
 }
--- src/router/guestbook.ts
+++ src/router/guestbook.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { GuestbookController } from '../controller/GuestbookController';
+import { AuthMiddleware } from '../middleware/AuthMiddleware';
 
 const routes = Router();
 
 routes.get('', GuestbookController.getGuestbook);
+routes.post('', AuthMiddleware.verifyToken, GuestbookController.createGuestbook);
 
 export default routes;
